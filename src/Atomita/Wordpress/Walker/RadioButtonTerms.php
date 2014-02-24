a139595550b4938c21c166c86f65c42be259d0c7
atomita <atom.at.omita@gmail.com>
2014-02-24 16:58:50 +0900

    version 0.0.2

--- src/Atomita/Wordpress/Walker/RadioButtonTerms.php
+++ src/Atomita/Wordpress/Walker/RadioButtonTerms.php
@@ -7,11 +7,33 @@ namespace Atomita\Wordpress\Walker;
  */
 class RadioButtonTerms extends \Walker_Category_Checklist {
 
-	function start_el(&$output, $category, $depth = 0, $args = array(), $id = 0){
+	var $taxonomy	   = null;
+	var $checked_ontop = true;
+
+	function start_el(&$output, $category, $depth = 0, $args = array(), $id = 0)
+	{
 		parent::start_el($output, $category, $depth, $args, $id);
 
 		$output = preg_replace('/ type="checkbox" /u', ' type="radio" ', $output);
 	}
+	
+	static function regist($taxonomy = 'category', $checked_ontop = false, $priority = 10)
+	{
+		$that = new static();
+		$that->taxonomy	 = $taxonomy;
+		$that->checked_ontop = $checked_ontop;
+		
+		add_filter('wp_terms_checklist_args', array($that, 'wp_terms_checklist_args'), $priority, 2);
+	}
+
+	function wp_terms_checklist_args($args, $post_id = null)
+	{
+		if ($this->taxonomy == $args['taxonomy']){
+			$args['checked_ontop'] = $this->checked_ontop;
+			$args['walker']	   = $this;
+		}
+		
+		return $args;
+	}
 
 }
-
